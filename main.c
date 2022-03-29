c876729f2719e2895899d867626153eafbd42f0f
Rekyrice <hhh20020126@126.com>
2022-03-29 19:38:29 +0800

    main1

---
+++ main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include "head.h"     
+int  main(void)
+{
+	int x=0,y=0;
+       printf("请输入两个数字a，b：\n");
+       scanf("%d %d",&x,&y);
+       printf("sum=%d\n",add(x,y));
+       printf("sub=%d\n",sub(x,y));
+       return 0;       
+}
